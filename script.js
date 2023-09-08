492aae4f222b3577c038cb0d432ac59904d92bef
junaedialfarizi <junaedi38@gmail.com>
2023-09-08 23:46:23 +0700

    latihan javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstName = 'Junaedi';
+let lastName = 'Alfarizi';
+let userName = 'Junaedi Alarizi';
+let age = 32;
+let email = 'junaedi38@gmail.com'
+let hoby = ['Travelling','Kuliner','Badminton','Main Gitar'];
+let job = 'Karywan Swasta';
+let phone = '085716493756';
+let isMarried = true;
+
+console.log('Halo nama saya'+" "+firstName+" "+lastName,'usiaku'+" "+age+" "+'tahun,',
+'profesi saya sebagai'+" "+job+".",'Salam kenal!');
+
+console.log('Saya memiliki dua hobi yang sangat saya sukai yaitu'+" "+hoby[0]+"&"+hoby[1]+"."+
+'Tapi saya juga memiliki hobi lainya seperti'+" "+hoby[2]+"&"+hoby[3]);
+
+console.log('Saya memiliki akun GitHub dengan nama username'+" "+userName+","+
+'dan kamu bisa menghubungi saya di'+" "+phone+" "+'atau'+" "+'email saya'+" "+
+'di'+" "+email);
